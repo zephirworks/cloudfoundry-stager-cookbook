672c979b668f3075598f0c0eb641612c966407a1
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-20 00:27:55 +0100

    Declare our dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,11 @@ license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry-stager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.99.0"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+%w{ cloudfoundry rbenv }.each do |cb|
+  depends cb
+end
