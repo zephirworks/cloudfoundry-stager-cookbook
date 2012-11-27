01b40309a33941433b9dd02649d71a4c52ad6421
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-27 23:28:59 +0100

    Cleanup: remove inline documentation, now that we have a nice README.

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,44 +19,23 @@
 
 include_attribute "cloudfoundry::directories"
 
-# Where to install the CloudFoundry code.
 default['cloudfoundry_stager']['vcap']['install_path'] = "/srv/vcap-stager"
-
-# Repository to use when fetching the CloudFoundry code.
 default['cloudfoundry_stager']['vcap']['repo']         = "https://github.com/cloudfoundry/stager.git"
-
-# Git reference to use when fetching the CloudFoundry code. Can be
-# either a specific sha or a reference such as `HEAD` or `master`.
 default['cloudfoundry_stager']['vcap']['reference']    = "206b1071298dc25dcfb8ca4f0004a2edbc3b0b7e"
 
 default['cloudfoundry_stager']['ruby_version'] = node['cloudfoundry']['ruby_1_9_2_version']
 
-# Log level for the stager.
 default['cloudfoundry_stager']['log_level'] = "info"
-
-# Where to write the stager's logs.
 default['cloudfoundry_stager']['log_file'] = File.join(node['cloudfoundry']['log_dir'], "stager.log")
-
-# Where to write the stager's pid.
 default['cloudfoundry_stager']['pid_file'] = File.join(node['cloudfoundry']['pid_dir'], "stager.pid")
 
-# 
 default['cloudfoundry_stager']['max_staging_duration'] = 120
-
-# 
 default['cloudfoundry_stager']['max_active_tasks'] = 10
-
-# 
 default['cloudfoundry_stager']['queues'] = ['staging']
 
-# 
 default['cloudfoundry_stager']['data_dir'] = File.join(node['cloudfoundry']['data_dir'], "stager")
-
-# 
 default['cloudfoundry_stager']['tmp_dir'] = File.join(node['cloudfoundry_stager']['data_dir'], "tmp")
-
-# 
 default['cloudfoundry_stager']['cache_dir'] = File.join(node['cloudfoundry_stager']['data_dir'], "package_cache", "ruby")
 
-# 
+# XXX unused?
 default['cloudfoundry_stager']['secure'] = false
