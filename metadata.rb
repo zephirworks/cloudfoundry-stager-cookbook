f31ffa7e61fb028ad75dc4b1bd1c7d3fe71b0620
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-08 08:11:27 +0100

    Release 1.0.4: bump dependencies for breaking changes.

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry-stager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "1.0.4"
 
 %w{ ubuntu }.each do |os|
   supports os
 end
 
-depends "cloudfoundry", "~> 1.1.2"
+depends "cloudfoundry", "~> 1.2.0"
 depends "rbenv"
